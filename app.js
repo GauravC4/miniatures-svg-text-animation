ef9976ff75f31064619b38116f6c798b8adb06d6
Gaurav Chemburkar <gaurav.mrc@gmail.com>
2020-06-06 21:39:34 +0530

    implicit ending removed from animate for chrome

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const paths = document.querySelectorAll("svg path");
 
 paths.forEach((path, index) => {
-  path.style.strokeDasharray = path.getTotalLength();
-  path.style.strokeDashoffset = path.getTotalLength();
-  path.animate([{ strokeDashoffset: 0, stroke: "white" }], {
-    duration: 3000 + index * 100,
-    easing: "ease",
-    fill: "forwards",
-    delay: index * 150,
-  });
+  let pathLen = path.getTotalLength();
+  path.style.strokeDasharray = pathLen;
+  path.style.strokeDashoffset = pathLen;
+  path.animate(
+    [
+      {
+        strokeDashoffset: pathLen,
+        stroke: "transparent",
+      },
+      {
+        strokeDashoffset: 0,
+        stroke: "white",
+      },
+    ],
+    {
+      duration: 3000 + index * 100,
+      easing: "ease",
+      fill: "forwards",
+      delay: index * 150,
+    }
+  );
 });
