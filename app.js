e06f428e49fedeb833cb616ac97e3f9c8dc05803
Gaurav Chemburkar <gaurav.mrc@gmail.com>
2020-06-06 22:01:34 +0530

    timeout before starting animation, demo added

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const paths = document.querySelectorAll("svg path");
 
-paths.forEach((path, index) => {
-  let pathLen = path.getTotalLength();
-  path.style.strokeDasharray = pathLen;
-  path.style.strokeDashoffset = pathLen;
-  path.animate(
-    [
+window.setTimeout(render, 1000);
+
+function render() {
+  paths.forEach((path, index) => {
+    let pathLen = path.getTotalLength();
+    path.style.strokeDasharray = pathLen;
+    path.style.strokeDashoffset = pathLen;
+    path.animate(
+      [
+        {
+          strokeDashoffset: pathLen,
+          stroke: "transparent",
+        },
+        {
+          strokeDashoffset: 0,
+          stroke: "white",
+        },
+      ],
       {
-        strokeDashoffset: pathLen,
-        stroke: "transparent",
-      },
-      {
-        strokeDashoffset: 0,
-        stroke: "white",
-      },
-    ],
-    {
-      duration: 3000 + index * 100,
-      easing: "ease",
-      fill: "forwards",
-      delay: index * 150,
-    }
-  );
-});
+        duration: 3000 + index * 100,
+        easing: "ease",
+        fill: "forwards",
+        delay: index * 150,
+      }
+    );
+  });
+}
